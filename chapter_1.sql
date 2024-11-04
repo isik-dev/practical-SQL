39879692a23bc96fcdfa1f3ee98d38b9f740ff0a
isik-dev <islom4ik.numanov@gmail.com>
2024-11-04 13:55:32 +0900

    chore: chapter_1 diy done

--- chapter_1.sql
+++ chapter_1.sql
@@ -29,9 +29,48 @@ VALUES ('Janet', 'Smith', 'F.D. Roosevelt HS', '2011-10-30', 36200),
 ('Kathleen', 'Roush', 'F.D. Roosevelt HS', '2010-10-22', 38500); 
 -- comic about ISO format date: https://xkcd.com/1179/ -- 
 
-
--- DIY --
+-- ################################################################### --
+-- ############################## DIY ################################ --
+-- ################################################################### --
 -- try to create a catalog of all animals at your local zoo. --
 -- one table to track the kinds of animals in the collection --
 -- another table to track the specifics on each animal --------
 
+-- create databse: zoo --
+CREATE DATABASE zoo;
+
+
+-- create a table: animal_collections --
+CREATE TABLE animal_collections (
+	id bigserial,
+	kind varchar(50),
+	population numeric
+);
+
+-- insert into animal_collections --
+INSERT INTO animal_collections (kind, population)
+VALUES ('lion', 2),
+('zebra', 1),
+('giraffe', 1),
+('shark', 1)
+
+
+-- create a table: animals --
+CREATE TABLE animals (
+	id bigserial,
+	name varchar(50),
+	sex varchar(10),
+	kind varchar(50),
+	mass numeric,
+	diet varchar(50),
+	dob date,
+	doa date
+);
+
+-- insert into animals
+INSERT INTO ANIMALS (name, sex, kind, mass, diet, dob, doa)
+VALUES ('Alex', 'MALE', 'lion', 150, 'carnivore', '2010-03-01', '2010-03-15'),
+('Nairobi', 'FEMALE', 'lion', 110, 'carnivore', '2015-11-11', '2015-12-01'),
+('Merlo', 'MALE', 'zebra', 130, 'herbivore', '2022-10-12', '2023-01-01'),
+('Kyle', 'MALE', 'giraffe', 211, 'herbivore', '2023-02-19', '2023-04-15'),
+('Dexter', 'MALE', 'shark', 300, 'carnivore', '2018-09-01', '2018-12-12');
